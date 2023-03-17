781d3889148c4d1de8b6d83b2155a29885b3e998
Damian Campbell <damiancampbell@Damian-ka-MBP.attlocal.net>
2023-03-17 18:09:09 -0400

    adding image name and age to Person component and importing person into list component

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Person = () => {
-  return <div>Person</div>;
+  return (
+    <div className="flex items-center gap-x-6">
+      <img
+        className="h-16 w-16 rounded-full"
+        src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+        alt=""
+      />
+      <div>
+        <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
+          Leslie Alexander
+        </h3>
+        <p className="text-sm font-semibold leading-6 text-indigo-600">
+          Co-Founder / CEO
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Person;
