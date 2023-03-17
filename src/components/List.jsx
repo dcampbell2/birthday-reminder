8a3234e7ee39d5645b0487204f15dfe2b903b59c
Damian Campbell <damiancampbell@Damian-ka-MBP.attlocal.net>
2023-03-17 18:03:45 -0400

    moving list of people into List component for reusability

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
-const List = () => {
-  return <div>List</div>;
+const List = ({ people }) => {
+  return (
+    <ul
+      role="list"
+      className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2"
+    >
+      <li>
+        <div className="flex items-center gap-x-6">
+          {people.map((person) => {})}
+          <img
+            className="h-16 w-16 rounded-full"
+            src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            alt=""
+          />
+          <div>
+            <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
+              Leslie Alexander
+            </h3>
+            <p className="text-sm font-semibold leading-6 text-indigo-600">
+              Co-Founder / CEO
+            </p>
+          </div>
+        </div>
+      </li>
+
+      {/*More people*/}
+    </ul>
+  );
 };
 
 export default List;
